8aa25cd4d6f568ce10d49d78011c2484aa90a117
AI SCIENCES <49445127+AISPUBLISHING@users.noreply.github.com>
2022-02-28 07:46:47 +0100

    Add files via upload

---
+++ pyinstaller_tutorial.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# !pip install pyinstaller
+
+
+# In[ ]:
+
+
+while(True):
+    a = int(input("Enter Vaue A: "))
+    b = int(input("Enter Vaue B: "))
+    print("Sum of A and B is :", a+b)
+    
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
